362af993c0f8b22f5daa6dfe4d02c3dcfc6b5749
Robin Daugherty <robin@robindaugherty.net>
2021-01-16 12:58:55 -0500

    Remove Main storyboard

---
+++ Unitime/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unitime
+//
+//  Created by Robin Daugherty on 2021-01-16.
+//
+
+import Cocoa
+
+let delegate = AppDelegate()
+NSApplication.shared.delegate = delegate
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
